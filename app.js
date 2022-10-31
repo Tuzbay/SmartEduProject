79de57dc272ccda3f78b0d68c09244ed29b16563
Tuzbay <t.uzbayyelce@gmail.com>
2022-11-01 00:32:29 +0300

    Project Finish

--- app.js
+++ app.js
@@ -1,27 +1,69 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const flash = require('connect-flash');
+const methodOverride = require('method-override');
+const pageRoute = require('./routes/pageRoute');
+const courseRoute = require('./routes/courseRoute');
+const categoryRoute = require('./routes/categoryRoute');
+const userRoute = require('./routes/userRoute');
 
 const app = express();
+
+// Connect MongoDB
+
+mongoose.connect('mongodb://127.0.0.1:27017/smartedu-db').then(() => {
+  console.log('MongoDB Connected Successfuly...');
+});
 
 // Templete Engine
 app.set('view engine', 'ejs');
 
-// Middlewares
+// Global Variable
 
+global.userIN = null;
+
+// Middlewares
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    secret: 'my_keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: 'mongodb://127.0.0.1:27017/smartedu-db',
+    }),
+  })
+);
+
+app.use(flash());
+
+app.use((req, res, next) => {
+  res.locals.flashMessages = req.flash();
+  next();
+});
+
+app.use(
+  methodOverride('_method', {
+    methods: ['POST', 'GET'],
+  })
+);
 
 // Routes
 
-app.get('/', (req, res) => {
-  res.status(200).render('index', {
-    page_name: "index"
-  });
+app.use('*', (req, res, next) => {
+  userIN = req.session.userID;
+  next();
 });
 
-app.get('/about', (req, res) => {
-    res.status(200).render('about', {
-        page_name: "about"
-    });
-  });
+app.use('/', pageRoute);
+app.use('/courses', courseRoute);
+app.use('/categories', categoryRoute);
+app.use('/users', userRoute);
 
 const port = 3000;
 
